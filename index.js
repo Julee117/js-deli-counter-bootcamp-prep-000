dc3d7fcbb1456e93dd5982b6d93257a651942d79
Julee117 <juliannne@gmail.com>
2017-09-29 22:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function takeANumber(katsDeliLine, name) {
   var num = katsDeliLine.push(name);
    return `Welcome, ${name}. You are number ${katsDeliLine.length} in line.`;
 }
+
+function nowServing(katsDeliLine) {
+  if (katsDeliLine.length >= 1) {
+    return katsDeliline.shift()
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
